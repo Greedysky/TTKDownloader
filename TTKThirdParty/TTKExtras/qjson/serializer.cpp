16e45c00eb3b2c2be130f02380905574d60b09bb
Greedysky <greedysky@163.com>
2021-08-17 09:53:51 +0800

    refactor: Remove deprecated function[201487]

--- TTKThirdParty/TTKExtras/qjson/serializer.cpp
+++ TTKThirdParty/TTKExtras/qjson/serializer.cpp
@@ -444,13 +444,6 @@ void Serializer::serialize( const QVariant &v, QIODevice* io, bool* ok)
   }
 }
 
-QByteArray Serializer::serialize( const QVariant &v)
-{
-  bool ok;
-
-  return serialize(v, &ok);
-}
-
 QByteArray Serializer::serialize( const QVariant &v, bool *ok)
 {
   TTK_D(Serializer);
