43287a489e2d075c832b094c424b18927131f326
Greedysky <greedysky@163.com>
2021-08-27 21:16:36 +0800

    refactor: Optimized deprecated module[532139]

--- TTKModule/TTKWidget/downloadWidgetCoreKits/downloadabstracttablewidget.cpp
+++ TTKModule/TTKWidget/downloadWidgetCoreKits/downloadabstracttablewidget.cpp
@@ -5,7 +5,11 @@ DownloadAbstractTableWidget::DownloadAbstractTableWidget(QWidget *parent)
     : QTableWidget(parent)
 {
     setAttribute(Qt::WA_TranslucentBackground);
+#if TTK_QT_VERSION_CHECK(6,2,0)
+    setFont(QFont(QStringList() << "Helvetica"));
+#else
     setFont(QFont("Helvetica"));
+#endif
     setColumnCount(3);
     setRowCount(0);
     setShowGrid(false);//Does not display the grid
