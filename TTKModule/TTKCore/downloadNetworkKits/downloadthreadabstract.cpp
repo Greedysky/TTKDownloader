60196488ecdf2b03d730f5100653dc586c85ee20
Greedysky <greedysky@163.com>
2023-01-30 15:08:25 +0800

    refactor: Optimize time sleep module[743210]

--- TTKModule/TTKCore/downloadNetworkKits/downloadthreadabstract.cpp
+++ TTKModule/TTKCore/downloadNetworkKits/downloadthreadabstract.cpp
@@ -1,6 +1,8 @@
 #include "downloadthreadabstract.h"
 #include "downloadsettingmanager.h"
-#if defined Q_OS_UNIX || defined Q_CC_MINGW
+#ifdef Q_CC_MSVC
+#  include <qt_windows.h>
+#elif defined Q_OS_UNIX || defined Q_CC_MINGW
 #  include <unistd.h>
 #endif
 #include <QSslError>
