18f165c27286e3ec5ea5a2a1d59d4f64e65aadf5
Greedysky <greedysky@163.com>
2023-11-13 21:26:54 +0800

    refactor: Optimize common ttktime module[963011]

--- TTKModule/TTKWidget/downloadWidgetKits/downloadlistitemwidget.cpp
+++ TTKModule/TTKWidget/downloadWidgetKits/downloadlistitemwidget.cpp
@@ -149,7 +149,7 @@ void DownloadListItemWidget::updateDownloadSpeed()
 
 QString DownloadListItemWidget::timeStandardization(qint64 time)
 {
-    TTKTime t(time, TTKTime::Entity::Second);
+    TTKTime t(time * 1000);
     return QString("%1:%2:%3").arg(QString::number(t.hour()).rightJustified(2, '0'),
                                    QString::number(t.minute()).rightJustified(2, '0'),
                                    QString::number(t.second()).rightJustified(2, '0'));
