1ceeb2aa48426b9016fc53703d8e3f686f297d7a
Greedysky <greedysky@163.com>
2023-12-27 22:29:10 +0800

    docs: Update to latest copyright format[343932]

--- TTKLanguage/cn.ts
+++ TTKLanguage/cn.ts
@@ -325,7 +325,7 @@
     </message>
     <message>
         <source>Mail:Greedysky@163.com greedyskyplus@google.com
-Copyright© 2015 - 2023 Greedysky All Rights Reserved
+Copyright© 2015 - 2024 Greedysky All Rights Reserved
 本软件仅供学习交流，不可用于商业用途，否则后果自负。</source>
         <translation type="unfinished"></translation>
     </message>
